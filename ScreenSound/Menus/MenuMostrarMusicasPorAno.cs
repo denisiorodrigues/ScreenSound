3e8581ec22b32d86274923008f42d7c3fa27e0ec
DenisioRodrigues <DenisioRodrigues@DIGIX-PE0DFJFK>
2025-02-27 18:01:25 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Menus/MenuMostrarMusicasPorAno.cs
@@ -0,0 +1,36 @@
+﻿using ScreenSound.Banco;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class MenuMostrarMusicasPorAno : Menu
+{
+    public override void Executar(DAL<Artista> artistaDAL)
+    {
+        base.Executar(artistaDAL);
+        ExibirTituloDaOpcao("Exibir detalhes do artista");
+        Console.Write("Digite o ano de lançamento da música: ");
+        string anoLancamento = Console.ReadLine()!;
+        var musicaDAL = new DAL<Musica>(new ScreenSoundContext());
+        var listaMusicasAnoLancamento = musicaDAL.ListarPor(a => a.AnoLancamento.Value == Convert.ToInt32(anoLancamento));
+        if (listaMusicasAnoLancamento.Any())
+        {
+            Console.WriteLine($"\nMúsicas do Ano {anoLancamento}:");
+            foreach (var musica in listaMusicasAnoLancamento)
+            {
+                musica.ExibirFichaTecnica();
+            }
+
+            Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        else
+        {
+            Console.WriteLine($"\nO ano {anoLancamento} não foi encontrada!");
+            Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
---
+++ ScreenSound/Modelos/Musica.cs
@@ -0,0 +1,29 @@
+﻿namespace ScreenSound.Modelos;
+
+public class Musica
+{
+    public Musica(string nome, int? anoLancamento)
+    {
+        Nome = nome;
+        AnoLancamento = anoLancamento;
+    }
+
+    public string Nome { get; set; }
+    public int Id { get; set; }
+
+    public int? AnoLancamento { get; set; }
+
+    public virtual Artista Artista { get; set; }
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {Nome} - {AnoLancamento}");
+      
+    }
+
+    public override string ToString()
+    {
+        return @$"Id: {Id}
+        Nome: {Nome}";
+    }
+}
