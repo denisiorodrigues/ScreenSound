04a4dc592230ce58d6a6b6edacc44404f2d63ec2
Denisio Rodrigues <denisio@ymail.com>
2025-02-28 11:42:59 -0300

    alterando a tabela musica pra permitid artita nulo

--- ScreenSound.Api/Program.cs
+++ ScreenSound.Api/Program.cs
@@ -14,6 +14,7 @@ builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =
 
 builder.Services.AddDbContext<ScreenSoundContext>();
 builder.Services.AddTransient<DAL<Artista>>();
+builder.Services.AddTransient<DAL<Musica>>();
 
 var app = builder.Build();
 
@@ -72,6 +73,72 @@ app.MapDelete("/Artistas/{id}", ([FromServices] DAL<Artista> dal, int id) =>
     }
 
     dal.Deletar(artistaADeletar);
+
+    return Results.NoContent();
+});
+
+
+// Musicas
+
+app.MapGet("/Musicas", ([FromServices] DAL<Musica> dal) =>
+{
+    return Results.Ok(dal.Listar());
+});
+
+app.MapGet("/Musicas/{nome}", ([FromServices] DAL<Musica> dal, string nome) =>
+{
+    var musica = dal.RecuperarPor(a => a.Nome.ToLower().Equals(nome.ToLower()));
+    if (musica == null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(musica);
+});
+
+app.MapGet("/Musicas/Artista/{artistaId}", ([FromServices] DAL<Musica> dal, int artistaId) =>
+{
+    var musicas = dal.ListarPor(a => a.Artista.Id == artistaId);
+    if (musicas == null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(musicas);
+});
+
+app.MapPost("/Musicas", ([FromServices] DAL<Musica> dal, [FromBody] Musica musica) =>
+{
+    dal.Adicionar(musica);
+
+    return Results.Ok();
+});
+
+app.MapPut("/Musicas", ([FromServices] DAL<Musica> dal, [FromBody] Musica musica) =>
+{
+    var musicaAAtualizar = dal.RecuperarPor(a => a.Id == musica.Id);
+    if (musicaAAtualizar == null)
+    {
+        return Results.NotFound();
+    }
+
+    musicaAAtualizar.Nome = musica.Nome;
+    musicaAAtualizar.AnoLancamento = musica.AnoLancamento;
+    musicaAAtualizar.Artista = musica.Artista;
+    dal.Atualizar(musicaAAtualizar);
+
+    return Results.Ok();
+});
+
+app.MapDelete("/Musicas/{id}", ([FromServices] DAL<Musica> dal, int id) =>
+{
+    var musicaADeletar = dal.RecuperarPor(a => a.Id == id);
+    if (musicaADeletar == null)
+    {
+        return Results.NotFound();
+    }
+
+    dal.Deletar(musicaADeletar);
 
     return Results.NoContent();
 });
--- ScreenSound.Modelos/Musica.cs
+++ ScreenSound.Modelos/Musica.cs
@@ -15,7 +15,9 @@ public class Musica
 
     public int? AnoLancamento { get; set; }
 
-    public virtual Artista Artista { get; set; }
+    public int? ArtistaId { get; set; }
+
+    public virtual Artista? Artista { get; set; }
 
     public void ExibirFichaTecnica()
     {
