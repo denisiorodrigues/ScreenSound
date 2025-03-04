6ed89848f046e001d711e41af6ea902b7b09c13c
Denisio Rodrigues <denisio@ymail.com>
2025-03-04 09:53:21 -0300

    Enpoints de genero

--- ScreenSound.Api/EndPoints/GeneroExtensions.cs
+++ ScreenSound.Api/EndPoints/GeneroExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ScreenSound.Api.Requests;
 using ScreenSound.Api.Response;
 using ScreenSound.Dados.Banco;
 using ScreenSound.Modelos;
@@ -11,7 +12,59 @@ public static class GeneroExtensions
     {
         app.MapGet("/Generos", ([FromServices] DAL<Genero> dal) =>
         {
-            return Results.Ok((dal.Listar().Select(genero => new GeneroResponse(genero.Nome, genero.Descricao))));
+            return Results.Ok(ConvertListGeneroToResponse(dal.Listar()));
         });
+
+        app.MapGet("/Generos/{nome}", ([FromServices] DAL<Genero> dal, string nome) =>
+        {
+            var generoEncontrado = dal.RecuperarPor(g => g.Nome.ToUpper().Equals(nome.ToUpper()));
+            
+            if (generoEncontrado is null) return Results.NotFound();
+
+            return Results.Ok(ConvertEntityToResponse(generoEncontrado));
+        });
+
+        app.MapPost("/Generos", ([FromServices] DAL<Genero> dal, GeneroRequest generoRequest) =>
+        {
+            var genero = new Genero() { Nome = generoRequest.Nome, Descricao = generoRequest.Descricao };
+            dal.Adicionar(genero);
+            return Results.Ok();
+        });
+
+        app.MapPut("/Generos", ([FromServices] DAL<Genero> dal, GeneroRequestEdit generoRequestEdit) =>
+        {
+            var generoEncontrado = dal.RecuperarPor(g => g.Nome.ToUpper().Equals(generoRequestEdit.Nome.ToUpper()));
+
+            if(generoEncontrado is null)
+                return Results.BadRequest("Genero não encontrado");
+
+            generoEncontrado.Nome = generoRequestEdit.Nome;
+            generoEncontrado.Descricao = generoRequestEdit.Descricao;
+            dal.Atualizar(generoEncontrado);
+
+            return Results.Ok();
+        });
+
+        app.MapDelete("/Generos", ([FromServices] DAL<Genero> dal, int id) =>
+        {
+            var generoEncontrado = dal.RecuperarPor(g => g.Id == id);
+
+            if (generoEncontrado is null)
+                return Results.BadRequest("Genero não encontrado");
+
+            dal.Deletar(generoEncontrado);
+
+            return Results.Ok();
+        });
+    }
+
+    private static object? ConvertListGeneroToResponse(IEnumerable<Genero> enumerable)
+    {
+        return enumerable.Select(g => ConvertEntityToResponse(g));
+    }
+
+    private static GeneroResponse ConvertEntityToResponse(Genero g)
+    {
+        return new GeneroResponse(g.Nome, g.Descricao);
     }
 }
