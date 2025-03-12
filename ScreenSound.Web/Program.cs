fe4cfe4ff1604e78c23a082518c3d2bc6cfa7257
Denisio Rodrigues <denisio@ymail.com>
2025-03-12 18:18:20 -0300

    criando as paginas do sistema

--- ScreenSound.Web/Program.cs
+++ ScreenSound.Web/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using ScreenSound.Web;
 using ScreenSound.Web.Service;
 
@@ -7,6 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddMudServices();
 builder.Services.AddTransient<ArtistaService>();
 
 builder.Services.AddHttpClient("ScreenSoundAPI", client => {
