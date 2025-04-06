a45e35468dd7ea0163d7e2b5626c3c5bb1936ebf
Denisio Rodrigues <denisio@ymail.com>
2025-04-06 19:39:35 -0300

    migracao de generos

---
+++ ScreenSound.Dados/Migrations/20250406210736_Popula-Tabela-Genero.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Dados.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaTabelaGenero : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Generos", new string[] { "Nome", "Descricao" }, new object[] { "MPB", "Musica popular braileira" });
+            migrationBuilder.InsertData("Generos", new string[] { "Nome", "Descricao" }, new object[] { "POP", "Musicas genero popular" });
+            migrationBuilder.InsertData("Generos", new string[] { "Nome", "Descricao" }, new object[] { "ROCK", "Musica underground" });
+            migrationBuilder.InsertData("Generos", new string[] { "Nome", "Descricao" }, new object[] { "Eletronica", "Musica instrumentros eletronicos" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Generos");
+        }
+    }
+}
---
+++ ScreenSound.Dados/Migrations/20250406214053_AtualizarArtistaIdMusicas.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Dados.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizarArtistaIdMusicas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Atualizar ""ArtistaId"" para Djavan
+            migrationBuilder.Sql(@"
+                UPDATE ""Musicas""
+                SET ""ArtistaId"" = 1
+                WHERE ""Nome"" IN ('Flor de Lis', 'Oceano', 'Se...')
+            ");
+
+            // Atualizar ""ArtistaId"" para Foo Fighters
+            migrationBuilder.Sql(@"
+                UPDATE ""Musicas""
+                SET ""ArtistaId"" = 2
+                WHERE ""Nome"" IN ('Everlong', 'Learn to Fly', 'Best of You', 'The Pretender', 'My Hero')
+            ");
+
+            // Atualizar ""ArtistaId"" para Pitty
+            migrationBuilder.Sql(@"
+                UPDATE ""Musicas""
+                SET ""ArtistaId"" = 3
+                WHERE ""Nome"" IN ('Máscara', 'Admirável Chip Novo', 'Me Adora', 'Equalize', 'Na Sua Estante')
+            ");
+
+            // Atualizar ""ArtistaId"" para Gilberto Gil
+            migrationBuilder.Sql(@"
+                UPDATE ""Musicas""
+                SET ""ArtistaId"" = 4
+                WHERE ""Nome"" IN ('Aquele Abraço', 'Andar com Fé', 'Palco', 'Vamos Fugir', 'Toda Menina Baiana')
+            ");
+
+            // Atualizar ""ArtistaId"" para Roque Abílio
+            migrationBuilder.Sql(@"
+                UPDATE ""Musicas""
+                SET ""ArtistaId"" = 7
+                WHERE ""Nome"" IN ('Coração de Boiadeiro', 'A Volta do Boiadeiro', 'O Rei do Gado', 'Peão Apaixonado', 'Viola Divina')
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE ""Musicas""
+                SET ""ArtistaId"" = NULL
+                WHERE ""ArtistaId"" IN (1, 2, 3, 4, 7)
+            ");
+        }
+    }
+}
